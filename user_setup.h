4ea10a8dedbec696a9c4287029602b4651cec971
gabrieeeel <gabriel.g.bittencourt-extern@renault.com>
2026-05-15 09:19:06 -0300

    Create user_setup.h

---
+++ user_setup.h
@@ -0,0 +1,12 @@
+#define ILI9488_DRIVER
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   15
+#define TFT_DC   2
+#define TFT_RST  4
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define SPI_FREQUENCY  27000000
