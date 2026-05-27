8270ad83b909072bc185bb4ef08ff9ec6b81a57c
gabrieeeel <gabriel.g.bittencourt-extern@renault.com>
2026-05-27 10:04:02 -0300

    bms_simu.cpp (27/05/2026)

--- bms_simu.cpp
+++ bms_simu.cpp
@@ -4,18 +4,69 @@
 
 // ---------- Pinos MCP2515 ----------
 #define CAN_CS    10
-#define CAN_MISO  11   // MCP SO
-#define CAN_MOSI  12   // MCP SI
-#define CAN_SCK   13
+#define CAN_MISO  13   // MCP SO
+#define CAN_MOSI  11   // MCP SI
+#define CAN_SCK   12
 #define CAN_INT   14
 
 // ---------- Objeto ----------
-MCP_CAN CAN(CAN_CS);
+SPIClass fspi2(FSPI);
+MCP_CAN CAN(&fspi2, CAN_CS);
+
+// Definição IDs CAN
+const unsigned long ID_WAKEUP   = 0x100;
+const unsigned long ID_WAKEUPON = 0x101;
+const unsigned long ID_SOC      = 0x200;
+const unsigned long ID_SOCR     = 0X201;
+
+// Frames
+byte FRAME_WAKEUP [8] = {0XAA, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00};
+byte FRAME_WAKEUPON [8] = {0XAB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+byte FRAME_SOC [8] = {0XBB, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00};
+byte FRAME_SOCR [8] = {0XBC, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00};
 
 // ---------- Estado ----------
-float tensao = 48.00;
-bool subindo = true;
-unsigned long ultimaAtualizacao = 0;
+
+void sendwakeupon() {
+  CAN.sendMsgBuf(ID_WAKEUPON, 0, 8, FRAME_WAKEUPON);
+}
+
+void sendsocr() {
+  CAN.sendMsgBuf(ID_SOCR, 0, 8, FRAME_SOCR);
+}
+
+void checkrequestwakeup() {
+
+  Serial.print("INT: ");
+  Serial.println(digitalRead(CAN_INT));
+
+    if(!digitalRead(CAN_INT)) {
+    long unsigned int rxId;
+    byte len;
+    byte rxBuf[8];
+    CAN.readMsgBuf(&rxId, &len, rxBuf);
+
+    Serial.print("Simulador recebeu ID: 0x");
+        Serial.println(rxId, HEX);
+  
+  if (rxId == ID_WAKEUP && rxBuf[0] == 0xAA) {
+    Serial.println("Enviando WAKEUPON...");
+    sendwakeupon(); 
+  }
+ }
+}
+
+ void checkrequestsoc() {
+  if(!digitalRead(CAN_INT)) {
+    long unsigned int rxId;
+    byte len;
+    byte rxBuf[8];
+    CAN.readMsgBuf(&rxId, &len, rxBuf);
+  
+  if (rxId == ID_SOC && rxBuf[0] == 0xBB)
+    sendsocr();
+ }
+ }
 
 // ---------- Setup ----------
 void setup() {
@@ -23,70 +74,26 @@ void setup() {
   delay(500);
 
   // ESP32-S3 precisa configurar pinos SPI
-  SPI.begin(CAN_SCK, CAN_MISO, CAN_MOSI, CAN_CS);
+  fspi2.begin(CAN_SCK, CAN_MISO, CAN_MOSI, CAN_CS);
 
   // ATENCAO: confira o cristal do MCP2515 (8 ou 16 MHz)
   if (CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
-    Serial.println("BMS Sim: MCP2515 OK");
+    Serial.println("BMS: MCP2515 OK");
   } else {
-    Serial.println("BMS Sim: ERRO no MCP2515");
+    Serial.println("BMS: ERRO no MCP2515");
     while (1);
   }
 
   CAN.setMode(MCP_NORMAL);
-  pinMode(CAN_INT, INPUT);
+  pinMode(CAN_INT, INPUT_PULLUP);
 
-  Serial.println("BMS Simulador pronto (request/response)");
+  Serial.println("BMS Simulador pronto (response)");
 }
 
 // ---------- Loop ----------
 void loop() {
-  // Atualiza tensao simulada (48-50V em onda triangular)
-  if (millis() - ultimaAtualizacao >= 100) {
-    ultimaAtualizacao = millis();
-    if (subindo) {
-      tensao += 0.05;
-      if (tensao >= 50.00) subindo = false;
-    } else {
-      tensao -= 0.05;
-      if (tensao <= 48.00) subindo = true;
-    }
-  }
-
-  // Escuta requests
-  if (!digitalRead(CAN_INT)) {
-    long unsigned int rxId;
-    byte len = 0;
-    byte rxBuf[8];
-    CAN.readMsgBuf(&rxId, &len, rxBuf);
-
-    if (rxId == 0x7E0 && len >= 1) {
-      byte pid = rxBuf[0];
-      byte resp[8] = {0};
-      resp[0] = pid;
-
-      switch (pid) {
-        case 0x01: {  // tensao
-          uint16_t raw = (uint16_t)(tensao * 100);
-          resp[1] = raw & 0xFF;
-          resp[2] = (raw >> 8) & 0xFF;
-          break;
-        }
-        default:
-          resp[1] = 0xFF;
-          break;
-      }
-
-      byte sndStat = CAN.sendMsgBuf(0x7E8, 0, 8, resp);
-      if (sndStat == CAN_OK) {
-        Serial.print("Respondi PID 0x");
-        Serial.print(pid, HEX);
-        Serial.print(" | tensao=");
-        Serial.print(tensao, 2);
-        Serial.println(" V");
-      } else {
-        Serial.println("Erro ao enviar");
-      }
-    }
-  }
+  checkrequestwakeup();
+  checkrequestsoc();
 }
+
+
